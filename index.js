e8a94b5edb8f43ff2ceb14ed7bc0574ce144ffd7
Uriel <CarrilloUriel81@gmail.com>
2024-01-03 19:35:41 -0800

    Player Gravity Implemented

--- index.js
+++ index.js
@@ -5,18 +5,24 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+
+const gravity = 0.2
 class Player {
     constructor() {
         this.position = {
             x: 100,
             y: 100
         }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
         this.width = 40;
         this.height = 40;
     }
 
     draw() {
-        c. fillStyle = 'blue';
+        c. fillStyle = 'blue'
         c. fillRect
         (this.position.x,
         this.position.y, 
@@ -24,7 +30,21 @@ class Player {
         this.height
             )
     }
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y;
+        
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+        this.velocity.y += gravity
+    else this.velocity.y = 0;
+    }
 }
 
 const player = new Player();
-player.draw()
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.update()
+}
+animate()
