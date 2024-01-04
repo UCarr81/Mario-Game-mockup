8b8bf5ea073aff0ea9b3e08dff1ef35d2ff7fb43
Uriel <CarrilloUriel81@gmail.com>
2024-01-03 20:05:04 -0800

    Added player Movement

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ class Player {
     update() {
         this.draw();
         this.position.y += this.velocity.y;
+        this.position.x += this.velocity.x;
         
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
         this.velocity.y += gravity
@@ -41,10 +42,72 @@ class Player {
 }
 
 const player = new Player();
+const keys = {
+    right: {
+        pressed: false,
+    },
+    left: {
+        pressed:false,
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.update()
+
+    if (keys.right.pressed) {
+        player.velocity.x = 5
+    } else if (keys.left.pressed) {
+        player.velocity.x = -5
+    } else player.velocity.x = 0
+    
 }
 animate()
+
+
+window.addEventListener('keydown', ({ keyCode }) => {
+    console.log(keyCode)
+    switch (keyCode) {
+        case 65: console.log('left');
+        keys.left.pressed = true;
+        break
+
+        case 87: console.log('up');
+        break
+
+        case 68: console.log('right');
+        keys.right.pressed = true;
+        break
+
+        case 82: console.log('down');
+        break
+
+        case 32: console.log('space');
+        break
+    }
+    console.log(keys.right.pressed)
+});
+
+window.addEventListener('keyup', ({ keyCode }) => {
+    console.log(keyCode)
+    switch (keyCode) {
+        case 65: console.log('left');
+        keys.left.pressed = false;
+        break
+
+        case 87: console.log('up');
+        break
+
+        case 68: console.log('right');
+        keys.right.pressed = false;
+        break
+
+        case 82: console.log('down');
+        break
+
+        case 32: console.log('space');
+        break
+    }
+    console.log(keys.right.pressed)
+});
